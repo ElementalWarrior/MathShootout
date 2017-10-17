008bbfb51f8172f8067d742b7f3ec206f252fcbc
danaack <danak@shaw.ca>
2017-10-16 17:45:49 -0700

    Implement enter team name
    Partial implementation of choosing team colours

---
+++ HockeyGameUnity/Assets/EnterTeamNameScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterTeamNameScript : MonoBehaviour {
+
+	public static string team_name;
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetString ("TeamName", GameObject.Find ("PathToTheGameObject").GetComponent<InputField> ().text);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
